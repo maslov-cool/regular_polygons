66f4b8881b40f670e4d63e9bf50685692b478e6a
alex <106116143+maslov-cool@users.noreply.github.com>
2024-05-13 01:48:10 +0300

    Update Правильные многоугольники.py
    Упрощаем расчёты

--- Правильные многоугольники.py
+++ Правильные многоугольники.py
@@ -5,8 +5,8 @@ import math
 class MyImageDraw(ImageDraw):
     def regular_polygon(self, center_coords, sides, radius, rotation=0, fill=None, outline=None):
         points = []
-        angle = -math.degrees(rotation)
-        while angle < 360 - math.degrees(rotation):
+        angle = math.degrees(rotation)
+        while angle < 360 + math.degrees(rotation):
             points.append((int(math.sin(math.radians(angle)) * radius + center_coords[0]),
                            int(math.cos(math.radians(angle)) * radius + center_coords[1])))
             angle += 360 / sides
